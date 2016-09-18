3a9d58cfeb5a0416816b2aea5a1d749cfb87ddad
Jeff Cooper <jeff@cuperman.net>
2016-09-18 08:00:39 -0700

    Use animations from animate.css

--- public/js/iframe-carousel.js
+++ public/js/iframe-carousel.js
@@ -31,25 +31,39 @@
     this.$el.hide();
     return this;
   };
-
-  Element.prototype.animateIn = function() {
+  
+  Element.prototype._animate = function(before, after) {
     var deferred = $.Deferred();
-    this.$el.fadeIn({
-      done: function() {
-        deferred.resolve();
-      }
+    this.$el.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+      deferred.resolve();
     });
+    if ($.isFunction(after)) {
+      deferred.then(after.bind(this));
+    }
+    if ($.isFunction(before)) {
+      before.call(this);
+    }
     return deferred.promise();
+  }
+
+  Element.prototype.animateIn = function(transition) {
+    var classed = ['animated', transition].join(' ');
+    return this._animate(function() {
+      this.$el.addClass(classed);
+      this.$el.show();
+    }, function() {
+      this.$el.removeClass(classed);
+    });
   };
 
-  Element.prototype.animateOut = function() {
-    var deferred = $.Deferred();
-    this.$el.fadeOut({
-      done: function() {
-        deferred.resolve();
-      }
+  Element.prototype.animateOut = function(transition) {
+    var classed = ['animated', transition].join(' ');
+    return this._animate(function() {
+      this.$el.addClass(classed);
+    }, function() {
+      this.$el.hide();
+      this.$el.removeClass(classed);
     });
-    return deferred.promise();
   };
 
   /**
@@ -78,14 +92,16 @@
    */
   var IframeCarousel = function(el, sources) {
     this.$el = $(el);
-    this.interval = 5000; // 5 seconds
-
     this.sources = sources || [];
-    this.visibleSourceIndex = 0;
-
+    this.interval = 5000; // 5 seconds
+    this.transitionIn = 'zoomInDown';
+    this.transitionOut = 'zoomOutDown';
+    
     if (this.sources.length < 2) {
       throw 'InsufficientSources';
     }
+
+    this.visibleSourceIndex = 0;
 
     this.container = new Element('div', { classed: 'carousel-container' });
     this.frames = [
@@ -132,8 +148,8 @@
 
   IframeCarousel.prototype.toggleFrames = function() {
     var promise = $.when(
-      this.visibleFrame.animateOut(),
-      this.hiddenFrame.animateIn()
+      this.visibleFrame.animateOut(this.transitionOut),
+      this.hiddenFrame.animateIn(this.transitionIn)
     );
 
     var tmp = this.visibleFrame;
